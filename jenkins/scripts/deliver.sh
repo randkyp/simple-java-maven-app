3755b549cf0d94270a277435ada08fc1beb08ab7
randkyp <95997939+randkyp@users.noreply.github.com>
2023-07-20 04:34:08 +0700

    add test text

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo '!!! TEST TEXT TO SEE IF JENKINS BUILDER CAN FETCH THIS !!!'
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
