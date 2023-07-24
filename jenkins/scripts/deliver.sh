7709c6ff8a3a68b0fd66eb5a0387b48f4d11a282
randkyp <95997939+randkyp@users.noreply.github.com>
2023-07-24 14:10:53 +0000

    3rd dcdg criteria

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# TODO: actual deployment stage
+
 echo 'Copying the JAR file to the workspace directory...'
 set -x
 cp target/${NAME}-${VERSION}.jar ./app.jar
 set +x
 
-// TODO: actual deployment stage
 echo "Delivery stage not implemented yet."
+
+# Wait for 1 minute on success
+set -x
+echo "Deployment successful. Pausing for 1 minute."
+sleep 60
+set +x
